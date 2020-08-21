e0849e85881338ec6f63d0cdb39d4420c7cf2efa
Suman11411 <49188734+Suman11411@users.noreply.github.com>
2020-08-21 08:01:49 +0530

    Update Hello java

--- Hello.java
+++ Hello.java
@@ -2,8 +2,10 @@ class Hello
 { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-    } 
+    public static void main(String args[])
+        string i=Hello Raghu...
+        for(i=0;i<=10;i++)
+        { 
+            System.out.println("Hello, World"); 
+        } 
 } 
