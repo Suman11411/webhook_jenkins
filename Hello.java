efff839b5790ca5c1a28822243dee24329bb5b8a
Suman11411 <49188734+Suman11411@users.noreply.github.com>
2020-08-21 08:04:27 +0530

    Update1

--- Hello.java
+++ Hello.java
@@ -4,8 +4,8 @@ class Hello
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[])
         string i=Hello Raghu...
-        for(i=0;i<=10;i++)
+        for(i = 0 ;i < 10; i++)
         { 
-            System.out.println("Hello, World"); 
+            System.out.println(i); 
         } 
 } 
