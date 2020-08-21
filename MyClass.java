f66ec4f690c99625bd20c4a9e1089d65b081179d
Suman11411 <49188734+Suman11411@users.noreply.github.com>
2020-08-21 08:17:23 +0530

    Myclass U1

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
   public static void main(String[] args) {
     for (int i = 0; i <= 10; i = i + 2) {
-      System.out.println("Hello Raghu ...");
+      System.out.println("Hello Raghu ....");
     }  
   }
 }
